5cd1155d83db1bd18ce2335d773813b9be4f57cb
loiclopez <loic.lopez@epitech.eu>
2018-03-12 22:10:53 +0100

    add api user test

---
+++ tests/API/APIUserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\API;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class APIUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
