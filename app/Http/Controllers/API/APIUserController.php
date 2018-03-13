258c90e37b967d6e3f39075d21060eef6120c1f8
Loic Dupil <loic.dupil@epitech.eu>
2018-03-13 14:45:03 +0100

    backend deployments

---
+++ app/Http/Controllers/API/APIUserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Validator;
+
+class APIUserController extends APIBaseController
+{
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|unique:users|email',
+            'first_name' => 'required|max:50|alpha',
+            'last_name' => 'required|max:50|alpha',
+            'nickname' => 'max:50',
+            'password' => 'required|max:255|confirmed'
+        ]);
+        if ($validator->fails()) {
+            return $this->response->errorBadRequest($validator->errors()->toJson());
+        }
+    }
+}
