e004b122c73466c96fb9f7f29777ce1e73107bfd
Yassir Jabbari <yassir.jabbari@epitech.eu>
2018-03-15 15:28:05 +0100

    Fix title

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>ExoSuite</title>
+    <title>ExoSuite - A propos</title>
 
     <!-- Fonts -->
     <link href="/css/page.min.css" rel="stylesheet">
