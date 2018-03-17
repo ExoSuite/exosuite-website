b0ace6af43b8d0f35d6436b266650f8d65f12349
loiclopez <loic.lopez@epitech.eu>
2018-03-17 11:21:31 +0100

    validator

--- app/Http/Controllers/API/APIUserController.php
+++ app/Http/Controllers/API/APIUserController.php
@@ -15,8 +15,8 @@ class APIUserController extends APIBaseController
             'email' => 'required|unique:users|email',
             'first_name' => 'required|max:50|alpha',
             'last_name' => 'required|max:50|alpha',
-            'nickname' => 'max:50',
-            'password' => 'required|max:255|confirmed'
+            'nickname' => 'min:4|max:50',
+            'password' => 'required|min:8|max:255|confirmed'
         ]);
         if ($validator->fails()) {
             return $this->response->errorBadRequest($validator->errors()->toJson());
