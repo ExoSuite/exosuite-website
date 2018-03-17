278871585812619049366937dc48e376bdf07f92
Loic Dupil <loic.dupil@epitech.eu>
2018-03-17 16:10:02 +0100

    LoginPasswordRule ajoutée et mise en place du Login Validator

--- app/Http/Controllers/API/APIAuthController.php
+++ app/Http/Controllers/API/APIAuthController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Rules\LoginPasswordRule;
+use Validator;
 
 class APIAuthController extends APIBaseController
 {
 
     public function login(Request $request)
     {
-
+        $validator = Validator::make($request->all(), [
+            'email' => "required|email|exists:users",
+            'password' =>
+                ["required", new LoginPasswordRule($request->get('password'), $request->get('email'))]
+        ]);
+        if ($validator->fails())
+            return $this->response->errorUnauthorized($validator->errors()->toJson());
+        return User::where("email", $request->get('email'))->first();
     }
 }
