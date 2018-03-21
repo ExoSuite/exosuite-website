74c7f76d14582bf9d791550c5ff584e368315d02
Yassir Jabbari <yassir.jabbari@epitech.eu>
2018-03-21 17:27:15 +0100

    Formulaire contact

---
+++ app/Http/Controllers/WebSite/ContactController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\WebSite;
+
+use Illuminate\Http\Request;
+use Mail;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contact')->with(array('mark' => true));
+    }
+
+    public function contact(Request $request)
+    {
+        $inputs = $request->all();
+        $this->validate($request, ['name' => 'required', 'email' => 'required|email', 'message' => 'required']);
+        Mail::send('emails.contact', array('inputs' => $inputs), function($message) use ($inputs)
+        {
+            $message->from($inputs['email'], 'ExoSuite');
+            $message->to('EIP.ExoSuite@gmail.com', 'ExoSuite')->subject('Message reçu du formulaire de contact du site');
+        });
+        return redirect('/');
+    }
+}
