59be49a46bd9bd8c8b387fe068b78d99e91b21fd
Loïc Lopez <loic.lopez@epitech.eu>
2018-10-02 18:49:34 -0400

    reformat dusk

--- dusk-phantomjs.sh
+++ dusk-phantomjs.sh
@@ -6,6 +6,6 @@ export PHANTOM_JS_PORT=${PORT}
 
 ./tests/bin/phantomjs --webdriver=${PORT} > /dev/null 2>&1 & > /dev/null 2>&1
 
-php artisan dusk
+./vendor/bin/phpunit --log-junit test-reports/phpunit.xml -c ./phpunit.dusk.xml
 
 lsof -i tcp:${PORT} | awk 'NR!=1 {print $2}' | xargs kill
