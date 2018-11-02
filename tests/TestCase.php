9d2ea9a123c0e242ac8b4cf7f195a73db38c60a1
Loïc Lopez <loic.lopez@epitech.eu>
2018-11-02 19:29:20 -0400

    test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,14 +4,24 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * Class TestCase
+ * @package Tests
+ */
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    /**
+     * @return void
+     */
     protected function french() : void {
         app()->setLocale('fr');
     }
 
+    /**
+     * @return void
+     */
     protected function english(): void {
         app()->setLocale('en');
     }
