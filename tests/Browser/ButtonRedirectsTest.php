02b98e82539407368832cf556a321a41b4b7991b
Loïc Lopez <loic.lopez@epitech.eu>
2019-01-13 16:24:17 -0500

    fix redirect

--- tests/Browser/ButtonRedirectsTest.php
+++ tests/Browser/ButtonRedirectsTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Browser;
 
-use Laravel\Dusk\Browser;
 use Tests\Browser\Config\RedirectConfig;
 use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 /**
  * Class ButtonRedirectsTest
@@ -27,7 +28,64 @@ class ButtonRedirectsTest extends DuskTestCase
         parent::setUp();
         $this->tests = [
             new RedirectConfig("get_home", trans('website.menu.home'), "get_home"),
+            new RedirectConfig("get_home", trans('website.menu.exosuite'), 'get_exosuite', trans('website.menu.project')),
+            new RedirectConfig("get_home", trans('website.menu.exofun'), 'get_exofun', trans('website.menu.project')),
+            new RedirectConfig("get_home", trans('website.menu.exorun'), 'get_exorun', trans('website.menu.project')),
+            new RedirectConfig("get_home", trans('website.menu.socialNetwork'), "get_social"),
+            new RedirectConfig("get_home", trans('website.menu.team'), "get_team"),
+            new RedirectConfig("get_home", trans('website.menu.pricing'), "get_pricing"),
+            new RedirectConfig("get_home", trans('website.menu.connect'), "login"),
+            new RedirectConfig("get_home", trans('website.menu.contact'), "get_contact"),
 
+            new RedirectConfig("get_contact", trans('website.menu.home'), "get_home"),
+            new RedirectConfig("get_contact", trans('website.menu.exosuite'), 'get_exosuite', trans('website.menu.project')),
+            new RedirectConfig("get_contact", trans('website.menu.exofun'), 'get_exofun', trans('website.menu.project')),
+            new RedirectConfig("get_contact", trans('website.menu.exorun'), 'get_exorun', trans('website.menu.project')),
+            new RedirectConfig("get_contact", trans('website.menu.socialNetwork'), "get_social"),
+            new RedirectConfig("get_contact", trans('website.menu.team'), "get_team"),
+            new RedirectConfig("get_contact", trans('website.menu.pricing'), "get_pricing"),
+            new RedirectConfig("get_contact", trans('website.menu.connect'), "login"),
+            new RedirectConfig("get_contact", trans('website.menu.contact'), "get_contact"),
+
+            new RedirectConfig("get_exofun", trans('website.menu.home'), "get_home"),
+            new RedirectConfig("get_exofun", trans('website.menu.exosuite'), 'get_exosuite', trans('website.menu.project')),
+            new RedirectConfig("get_exofun", trans('website.menu.exofun'), 'get_exofun', trans('website.menu.project')),
+            new RedirectConfig("get_exofun", trans('website.menu.exorun'), 'get_exorun', trans('website.menu.project')),
+            new RedirectConfig("get_exofun", trans('website.menu.socialNetwork'), "get_social"),
+            new RedirectConfig("get_exofun", trans('website.menu.team'), "get_team"),
+            new RedirectConfig("get_exofun", trans('website.menu.pricing'), "get_pricing"),
+            new RedirectConfig("get_exofun", trans('website.menu.connect'), "login"),
+            new RedirectConfig("get_exofun", trans('website.menu.contact'), "get_contact"),
+
+            new RedirectConfig("get_exorun", trans('website.menu.home'), "get_home"),
+            new RedirectConfig("get_exorun", trans('website.menu.exosuite'), 'get_exosuite', trans('website.menu.project')),
+            new RedirectConfig("get_exorun", trans('website.menu.exofun'), 'get_exofun', trans('website.menu.project')),
+            new RedirectConfig("get_exorun", trans('website.menu.exorun'), 'get_exorun', trans('website.menu.project')),
+            new RedirectConfig("get_exorun", trans('website.menu.socialNetwork'), "get_social"),
+            new RedirectConfig("get_exorun", trans('website.menu.team'), "get_team"),
+            new RedirectConfig("get_exorun", trans('website.menu.pricing'), "get_pricing"),
+            new RedirectConfig("get_exorun", trans('website.menu.connect'), "login"),
+            new RedirectConfig("get_exorun", trans('website.menu.contact'), "get_contact"),
+
+            new RedirectConfig("get_exosuite", trans('website.menu.home'), "get_home"),
+            new RedirectConfig("get_exosuite", trans('website.menu.exosuite'), 'get_exosuite', trans('website.menu.project')),
+            new RedirectConfig("get_exosuite", trans('website.menu.exofun'), 'get_exofun', trans('website.menu.project')),
+            new RedirectConfig("get_exosuite", trans('website.menu.exorun'), 'get_exorun', trans('website.menu.project')),
+            new RedirectConfig("get_exosuite", trans('website.menu.socialNetwork'), "get_social"),
+            new RedirectConfig("get_exosuite", trans('website.menu.team'), "get_team"),
+            new RedirectConfig("get_exosuite", trans('website.menu.pricing'), "get_pricing"),
+            new RedirectConfig("get_exosuite", trans('website.menu.connect'), "login"),
+            new RedirectConfig("get_exosuite", trans('website.menu.contact'), "get_contact"),
+
+            new RedirectConfig("get_pricing", trans('website.menu.home'), "get_home"),
+            new RedirectConfig("get_pricing", trans('website.menu.exosuite'), 'get_exosuite', trans('website.menu.project')),
+            new RedirectConfig("get_pricing", trans('website.menu.exofun'), 'get_exofun', trans('website.menu.project')),
+            new RedirectConfig("get_pricing", trans('website.menu.exorun'), 'get_exorun', trans('website.menu.project')),
+            new RedirectConfig("get_pricing", trans('website.menu.socialNetwork'), "get_social"),
+            new RedirectConfig("get_pricing", trans('website.menu.team'), "get_team"),
+            new RedirectConfig("get_pricing", trans('website.menu.pricing'), "get_pricing"),
+            new RedirectConfig("get_pricing", trans('website.menu.connect'), "login"),
+            new RedirectConfig("get_pricing", trans('website.menu.contact'), "get_contact")
         ];
     }
 
