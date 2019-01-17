a07309999301fe2270e9788c419d15d120fdff39
Yassir Jabbari <yassir.jabbari@exosuite.fr>
2019-01-16 23:56:23 -0500

    Edit profile fonctionnel

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Facades\API;
+
 class SocialController extends Controller
 {
     public function profile()
     {
-        return view('social.profile');
+        $access_token = session()->get('access_token');
+        $response = API::get('/user/me', [], ['Authorization' => 'Bearer ' . $access_token]);
+        return view('social.profile')->with(array('profile' => $response));
     }
 
     public function home()
     {
-        return view('social.newsfeed');
+        $access_token = session()->get('access_token');
+        $response = API::get('/user/me', [], ['Authorization' => 'Bearer ' . $access_token]);
+        return view('social.newsfeed')->with(array('profile' => $response));
     }
 }
